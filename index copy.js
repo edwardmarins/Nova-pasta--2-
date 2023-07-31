3cfdbf7387200b409595dd6bb1cf134c7c4ced7f
Carlos Eduardo Ferreira Marins <edwardmarins@gmail.com>
2023-07-31 18:17:30 -0300

    first commit

---
+++ index copy.js
@@ -0,0 +1,40 @@
+//var images=[
+
+//"https://e7.pngegg.com/pngimages/419/769/png-clipart-logos-gg-brand-design-text-trademark-thumbnail.png",
+//"https://e7.pngegg.com/pngimages/99/648/png-clipart-huawei-mediapad-m3-%E5%8D%8E%E4%B8%BA-huawei-mediapad-t3-7-huawei-mediapad-t3-10-lte-gg-gadget-mobile-phone-thumbnail.png"
+//];
+
+//function next(){
+    //Document.getElementById("gg").src=images[i];
+     //                               i++;}
+//var i = 0; function proximoslide() { document.getElementById("album").src=images[i]; i++; }
+
+function calc(){
+    var resultado;
+    Number(document.getElementById("g1").value)+(document.getElementById("g2").value);
+    console.log(resultado);
+    document.getElementById("t1").innerHTML = resultado
+}
+
+function calc1(){
+    var resultado;
+    Number(document.getElementById("g1").value)-(document.getElementById("g2").value);
+    console.log(resultado);
+    document.getElementById("t1").innerHTML = resultado
+}
+
+function calc2(){
+    var resultado;
+    Number(document.getElementById("g1").value)/(document.getElementById("g2").value);
+    console.log(resultado);
+    document.getElementById("t1").innerHTML = resultado
+}
+
+function calc3(){
+    var resultado;
+    Number(document.getElementById("g1").value)*(document.getElementById("g2").value);
+    console.log(resultado);
+    document.getElementById("t1").innerHTML = resultado
+ }
+ 
+
